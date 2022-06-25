0ac1b2b1f6529c7a50f111d17c208ffec181b64c
Stanimir <stanimirkosev00@gmail.com>
2022-06-25 21:49:12 +0300

    validation of sign-up form

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ var express = require("express");
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
 var router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 const User = require("../models/user");
 
@@ -9,53 +10,76 @@ const User = require("../models/user");
 router.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.errors = req.flash("error");
-  //res.locals.infos = req.flash("info");
+  res.locals.validationErr = validationResult(req).array();
   next();
 });
 
 /* messages before login */
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
   res.render("index");
 });
 
 /* messages after login */
-router.get("/members", function (req, res, next) {
+router.get("/members", function (req, res) {
   res.render("members");
 });
 
-router.get("/sign-up", function (req, res, next) {
+router.get("/sign-up", function (req, res) {
   res.render("signup");
 });
 
-router.post("/sign-up", function (req, res, next) {
-  var username = req.body.username;
+router.post(
+  "/sign-up",
+  // sanitization and validation
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Username must be specified."),
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Password must be specified."),
+  body(
+    "confirmPassword",
+    "Confirm password field must have the same value as the password field."
+  ).custom((value, { req }) => value === req.body.password),
 
-  // check if the same username exist
-  User.findOne({ username: username }, function (err, user) {
-    if (err) {
-      return next(err);
+  (req, res, next) => {
+    // if errors from above
+    if (!validationResult(req).isEmpty()) {
+      res.render("signup", { validationErr: validationResult(req).array() });
     }
-    if (user) {
-      req.flash("error", "Username alreay exists.");
-      return res.redirect("/sign-up");
-    }
-    // hash password
-    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+
+    // check if the same username exist
+    User.findOne({ username: req.body.username }, function (err, user) {
       if (err) {
         return next(err);
       }
-      const user = new User({
-        username: username,
-        password: hashedPassword,
-      }).save((err) => {
+      if (user) {
+        req.flash("error", "Username alreay exists.");
+        return res.redirect("/sign-up");
+      }
+
+      // hash password
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
         if (err) {
           return next(err);
         }
-        next();
+        const user = new User({
+          username: req.body.username,
+          password: hashedPassword,
+        }).save((err) => {
+          if (err) {
+            return next(err);
+          }
+          next();
+        });
       });
     });
-  });
-});
+  }
+);
 
 router.post(
   "/sign-up",
@@ -66,7 +90,7 @@ router.post(
   })
 );
 
-router.get("/log-in", function (req, res, next) {
+router.get("/log-in", function (req, res) {
   res.render("login");
 });
 
